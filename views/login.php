423fdff2b38b8fcbf5c3d573a1e934d4459ed5f1
evgeniy-lyashenko <e.n.lyashenko.1992@gmail.com>
2022-08-22 14:57:58 +0300

    'Uploading'

---
+++ views/login.php
@@ -0,0 +1,25 @@
+<h1>Login</h1>
+<form action="" method="post">
+    <div class="mb-3">
+        <label for="firstname" class="form-label">Firstname</label>
+        <input type="text" name="firstname" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label for="lastname" class="form-label">Lastname</label>
+        <input type="text" name="lastname" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" name="email" class="form-control">
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" name="password" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label for="confirmPassword" class="form-label">Confirm Password</label>
+        <input type="password" name="confirmPassword" class="form-control">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
